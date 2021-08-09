853a975d27a0b776c17a824471638f3b473c9c18
Faith Idowu <faithid10@gmail.com>
2021-08-09 07:28:00 +0100

    :art: chore: create dynamic pages

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,34 +1,30 @@
 import React from "react";
-import Layout from "../components/layout";
-import { useStaticQuery, graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../../components/layout";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 const BlogPage = () => {
   const blogPosts = useStaticQuery(graphql`
     query {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         nodes {
-          id
+          slug
           frontmatter {
             title
             date(formatString: "MMMM D, YYYY")
           }
-          body
         }
       }
     }
   `);
-
-  console.log({ blogPosts });
-
   return (
     <Layout pageTitle="My posts">
       <p>Cool posts come here</p>
       {blogPosts.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>{node.body}</MDXRenderer>
         </article>
       ))}
     </Layout>
